4a36330bf0f40d44f26a6de71bbe970833ddf99d
mashukmashum <mashukgce@gmail.com>
2018-10-09 15:22:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ wss.on('connection', (ws) => {
    // wss.clients.forEach(function(client) {
   //    client.send(message.utf8Data);
   //  });
-   ws.send('echo: ' + message);
+   wss.send('echo: ' + message);
   });
 
 
