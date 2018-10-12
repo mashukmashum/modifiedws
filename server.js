d6a1b8873824b8cb84f4ca24d71fec1db420cd76
mashukmashum <mashukgce@gmail.com>
2018-10-12 18:12:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
   
   ws.on('message', (mess) => {
-   var objm = mess.data;
+   var objm = mess;
     var obj=JSON.parse(objm);
     var mtype= obj.type;
     var uname= obj.uname;
